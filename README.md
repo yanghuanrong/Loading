c45dc34befb9978aa2d677128b4b3f56d9a9254e
老前端 <@DESKTOP-22C0FCR>
2017-04-13 11:48:02 +0800

    优化代码，编写dome

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# Loading
-集图片字体声音与一体的预加载。
+编写了一个集合了字体，声音，图片的web预加载。适合在移动端使用。<br>
+### demo中的load根据需求自己更改<br>
+```javascript
+load({
+    data:loadarry,
+    conduct:function(i){
+    	loadMove(i);
+    },
+    success:function(){
+      All.innerHTML='全部加载完成';
+      Music.play();
+    }
+})
+```
+### load提供了三个选项
+* data 要加载的内容
+* conduct 单个文件加载完成的回调函数
+* success 全部加载完成的回调函数
+### data内容格式示范<br>
+```javascript
+var loadarry=[
+	{
+		name:'图片',
+		type:'img',
+		src:'http://www.bz55.com/uploads/allimg/140928/1-14092Q21316.jpg'
+	},{
+		name:'造字工房悦黑细体',
+		type:'font',
+		src:'造字工房悦黑细体'
+	},{
+		name:'亡灵序曲',
+		type:'audio',
+		src:'http://www.internetke.com/jsEffects/2014101006/mp3/The Dawn.mp3'
+	}
+]
+```
+### 特别注意<br>
+加载font的时候，css但中必须要使用 @font-face 调用字体。 如下
+```css
+@font-face{font-family:'造字工房悦黑细体';src:url('font/造字工房悦黑细体.otf') format('truetype');}
+```
--- js/loading.js
+++ js/loading.js
@@ -13,72 +13,70 @@ font类型 css必须配置
 @font-face{font-family:'华康海报体W12';src:url('font/华康海报体W12.TTF') format('truetype');}
 @font-face{font-family:'简大黑';src:url('font/简大黑.TTF') format('truetype');}
 @font-face{font-family:'造字工房悦黑细体';src:url('font/造字工房悦黑细体.otf') format('truetype');}
-
  * */
-function Loading() {
-  this.init.apply(this, arguments)
-}
-Loading.prototype.init = function(aload, conduct, success) {
-  var iCount = aload.length,
-    iLoaded = 0,
-    num = 0;
-  for(var i = 0; i < iCount; i++) {
-    (function(i) {
-      switch(aload[i].type) {
-        case 'img':
-          var oImg = new Image();
-          oImg.onload = function() {
-            loadfun(i);
-            this.onload = null;
-          }
-          oImg.src = aload[i].src;
-          break;
-        case 'font':
-          document.fonts.load('1em ' + aload[i].src).then(function() {
-            loadfun(i);
-          }, function() {
-            console.log(aload[i].src + '字体加载失败');
-          });
-          break;
-        case 'mp3':
-          var oAudio = new Audio();
-          oAudio.onloadedmetadata = function() {
-            loadfun(i);
-            this.onload = null;
-          }
-          oAudio.src = aload[i].src
-          break;
+! function(w, d) {
+	function Z(opt) {
+		this.init(opt.data, opt.conduct, opt.success)
+	}
+	Z.prototype.init = function(loadAll, conduct, success) {
+		var iCount = loadAll.length,
+			iLoaded = 0,
+			num = 0;
+		for(var i = 0; i < iCount; i++) {
+			(function(i) {
+				switch(loadAll[i].type) {
+					case 'img':
+						var oImg = new Image();
+						oImg.onload = function() {
+							loadfun(i);
+							this.onload = null;
+						}
+						oImg.src = loadAll[i].src;
+						break;
+					case 'font':
+						document.fonts.load('1em ' + loadAll[i].src).then(function() {
+							loadfun(i);
+						}, function() {
+							console.log(loadAll[i].src + 'font timeout');
+						});
+						break;
+					case 'audio':
+						var oAudio = new Audio();
+						oAudio.onloadedmetadata = function() {
+							loadfun(i);
+							this.onload = null;
+						}
+						oAudio.src = loadAll[i].src
+						break;
 
-        default:
-          console.log('load格式错误');
-          return false;
-          break;
-      }
-    })(i);
-  }
+					default:
+						console.log('Load timeout');
+						return false;
+						break;
+				}
+			})(i);
+		}
 
-  var loadfun = function(i) {
-    num = Math.ceil(++iLoaded / iCount * 100);
-    if(typeof(conduct) == 'function') {
-      conduct({
-        name: aload[i].name,
-        type: aload[i].type,
-        num: num,
-        src: aload[i].src
-      });
-    }
-    if(num == '100') {
-      if(typeof(success) == 'function') {
-        success(num);
-      }
-    }
-  }
-}
+		function loadfun(i) {
+			num = Math.ceil(++iLoaded / iCount * 100);
+			if(typeof(conduct) == 'function') {
+				conduct({
+					name: loadAll[i].name,
+					type: loadAll[i].type,
+					num: num,
+					src: loadAll[i].src
+				});
+			}
+			if(num == '100') {
+				if(typeof(success) == 'function') {
+					success(num);
+				}
+			}
+		}
+	}
 
-function load(opt,arguments) {
-  if(arguments!=undefined){
-    return new Loading(opt,arguments);
-  }else{
-    return new Loading(opt.id, opt.fileok, opt.sussess);
-  }
-}
+	function z(opt) {
+		return new Z(opt);
+	}
+	window.load = z;
+}(window, document);
